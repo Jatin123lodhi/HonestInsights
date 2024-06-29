df5f0f8f7a0b3b50d0c8babfb56592f1aa4bf1d7
Jatin Lodhi <jatinlodhi111@gmail.com>
2024-06-29 17:23:16 +0530

    formated data using dayjs

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -12,6 +12,7 @@ import { Message } from '@/model/User'
 import axios, { AxiosError } from 'axios'
 import { useToast } from './ui/use-toast'
 import { ApiResponse } from '@/types/ApiResponse'
+import dayjs from 'dayjs';
 
 type MessageCardProps = {
   message: Message;
@@ -44,10 +45,10 @@ const MessageCard = ({message,onDeleteSuccess}: MessageCardProps) => {
       <div className='flex justify-between'>
       <CardHeader className=''>
         <CardTitle>{message?.content ?? "Some Message Title"}</CardTitle>
-        <CardDescription>{message?.createdAt?.toLocaleString() ?? "2 hours ago"}</CardDescription>
+        <CardDescription className='pt-2'>{dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}</CardDescription>
       </CardHeader>
       <div className=' p-5'>
-        <AlertDialogComp handleContinue={handleDeleteConfirm} onMessageDelete={()=>console.log('hihi')} />
+        <AlertDialogComp handleContinue={handleDeleteConfirm} />
       </div>
       </div>
     </Card>
