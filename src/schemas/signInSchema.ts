044a5ceb643cd0491bc619dc7c75f3ab51e4ab1c
Jatin Lodhi <jatinlodhi111@gmail.com>
2024-06-29 17:10:58 +0530

    added error messages

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  identifier: z.string(),
-  password: z.string(),
+  identifier: z.string().min(4,'Min length should be atleast 4 character').max(20,'Max allowed length is 20 character'),
+  password: z.string().min(4,'Min length should be atleast 4 character').max(20,'Max allowed length is 20 character'),
 });
