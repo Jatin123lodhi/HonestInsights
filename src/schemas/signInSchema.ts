0bbca588ff912168c7ab73e451574dbd73769e51
Jatin Lodhi <jatinlodhi111@gmail.com>
2024-07-06 15:13:03 +0530

    schema fixed max length

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  identifier: z.string().min(4,'Min length should be atleast 4 character').max(20,'Max allowed length is 20 character'),
+  identifier: z.string().min(4,'Min length should be atleast 4 character').max(60,'Max allowed length is 60 character'),
   password: z.string().min(4,'Min length should be atleast 4 character').max(20,'Max allowed length is 20 character'),
 });
