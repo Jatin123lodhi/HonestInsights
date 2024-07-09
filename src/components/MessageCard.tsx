1335f0c5ab6a43021d18588a4e2ea59cd0ad6a85
Jatin Lodhi <jatinlodhi111@gmail.com>
2024-07-09 07:03:48 +0530

    delete btn fix

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Card,
   CardContent,
@@ -6,54 +6,54 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { AlertDialogComp } from './AlertDialogComp'
-import { Message } from '@/model/User'
-import axios, { AxiosError } from 'axios'
-import { useToast } from './ui/use-toast'
-import { ApiResponse } from '@/types/ApiResponse'
-import dayjs from 'dayjs';
+} from "@/components/ui/card";
+import { AlertDialogComp } from "./AlertDialogComp";
+import { Message } from "@/model/User";
+import axios, { AxiosError } from "axios";
+import { useToast } from "./ui/use-toast";
+import { ApiResponse } from "@/types/ApiResponse";
+import dayjs from "dayjs";
 
 type MessageCardProps = {
   message: Message;
-  onDeleteSuccess: ()=>void
-}
+  onDeleteSuccess: () => void;
+};
 
-const MessageCard = ({message,onDeleteSuccess}: MessageCardProps) => {
-  const {toast} = useToast();
-  const handleDeleteConfirm = async()=>{
-    try{
-
-      const response = await axios.delete(`/api/delete-message/${message._id}`)
+const MessageCard = ({ message, onDeleteSuccess }: MessageCardProps) => {
+  const { toast } = useToast();
+  const handleDeleteConfirm = async () => {
+    try {
+      const response = await axios.delete(`/api/delete-message/${message._id}`);
       toast({
-        title: response.data.message
-      })
+        title: response.data.message,
+      });
       onDeleteSuccess();
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
       const axiosError = error as AxiosError<ApiResponse>;
-      if (axiosError.response ) {
-         toast({
-          title: 'Error',
-          description: axiosError.response.data.message
-         })
-      }  
-    } 
-  }
+      if (axiosError.response) {
+        toast({
+          title: "Error",
+          description: axiosError.response.data.message,
+        });
+      }
+    }
+  };
   return (
-    <Card className='max-w-[620px]'>
-      <div className='flex justify-between'>
-      <CardHeader className=''>
-        <CardTitle>{message?.content ?? "Some Message Title"}</CardTitle>
-        <CardDescription className='pt-2'>{dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}</CardDescription>
-      </CardHeader>
-      <div className='p-5'>
-        <AlertDialogComp handleContinue={handleDeleteConfirm} />
-      </div>
+    <Card className="max-w-[620px] ">
+      <div className="relative flex justify-between sm:p-2">
+        <CardHeader className="">
+          <CardTitle>{message?.content ?? "Some Message Title"}</CardTitle>
+          <CardDescription className="pt-2">
+            {dayjs(message.createdAt).format("MMM D, YYYY h:mm A")}
+          </CardDescription>
+        </CardHeader>
+        <div className="absolute -right-2 -top-2">
+          <AlertDialogComp handleContinue={handleDeleteConfirm} />
+        </div>
       </div>
     </Card>
+  );
+};
 
-  )
-}
-
-export default MessageCard
+export default MessageCard;
